a6f84f5eaa3cd6139c5acea086546edfaa194ac6
Jaehyung Lee <jaehyung.lee@sf.snu.ac.kr>
2023-05-23 13:59:33 +0900

    revise assignment

--- assignments/4-Equiv_Hoare/P01.v
+++ assignments/4-Equiv_Hoare/P01.v
@@ -1,7 +1,7 @@
 Require Export D.
 
 Theorem loop_never_stops : forall st st',
-  ~ ( Some st =[ Y := 0; while X <= Y do X := X - Y end ]=> st').
+  ~ ( Some st =[ Y := 0; while Y <= X do X := X - Y end ]=> st').
 Proof.
   exact FILL_IN_HERE.
 Qed.
